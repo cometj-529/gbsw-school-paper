e716d7bf37551afd5e681f0aada582e8ef8ab922
wjdgotjd529 <wjdgotjd529@naver.com>
2024-05-25 17:48:43 +0900

    프로젝트 세팅

---
+++ src/main/java/kr/hs/gbsw/schoolpaper/SchoolPaperApplication.java
@@ -0,0 +1,13 @@
+package kr.hs.gbsw.schoolpaper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolPaperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolPaperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/hs/gbsw/schoolpaper/SchoolPaperApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.hs.gbsw.schoolpaper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolPaperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
