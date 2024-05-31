ce92fc5f1836e4960a8ad9010dafe84cc06d6781
wjdgotjd <wjdgotjd529@naver.com>
2024-05-31 20:43:25 +0900

    로그인 정보 어노테이션 추가

---
+++ src/main/java/kr/hs/gbsw/schoolpaper/common/AuthUserId.java
@@ -0,0 +1,14 @@
+package kr.hs.gbsw.schoolpaper.common;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.PARAMETER)
+@AuthenticationPrincipal(expression = "#this == 'anonymousUser' ? null : #this")
+public @interface AuthUserId {
+}
