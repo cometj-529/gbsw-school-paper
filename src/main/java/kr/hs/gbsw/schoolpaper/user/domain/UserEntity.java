99244250b57615fe23e52e899513279949b99cdf
wjdgotjd <wjdgotjd529@naver.com>
2024-05-31 20:39:00 +0900

    User Id UUID로 변경

--- src/main/java/kr/hs/gbsw/schoolpaper/user/domain/UserEntity.java
+++ src/main/java/kr/hs/gbsw/schoolpaper/user/domain/UserEntity.java
@@ -1,12 +1,16 @@
-package kr.hs.gbsw.schoolpaper.user.entity;
+package kr.hs.gbsw.schoolpaper.user.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import kr.hs.gbsw.schoolpaper.student.domain.StudentEntity;
+import kr.hs.gbsw.schoolpaper.teacher.domain.TeacherEntity;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Entity
@@ -15,22 +19,34 @@ import java.util.stream.Collectors;
 @Setter
 public class UserEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
-
-    private String name;
+    private UUID uuid;
 
     @Column(unique = true)
     private String tel;
 
+    @JsonIgnore
     private String password;
 
-    @ManyToMany
+    @ManyToMany(cascade = {CascadeType.REMOVE})
     private List<RoleEntity> roles;
+
+    @OneToOne()
+    private StudentEntity student;
+
+    @OneToOne()
+    private TeacherEntity teacher;
 
     public List<GrantedAuthority> generateGrantedAuthorities() {
         return roles.stream()
                 .map(role -> new SimpleGrantedAuthority("ROLE_" + role.getId()))
                 .collect(Collectors.toList());
+    }
+
+    public boolean isStudent() {
+        return student != null;
+    }
+
+    public boolean isTeacher() {
+        return teacher != null;
     }
 }
--- src/main/java/kr/hs/gbsw/schoolpaper/user/service/UserService.java
+++ src/main/java/kr/hs/gbsw/schoolpaper/user/service/UserService.java
@@ -2,33 +2,40 @@ package kr.hs.gbsw.schoolpaper.user.service;
 
 import kr.hs.gbsw.schoolpaper.jwt.IdPwAuthentication;
 import kr.hs.gbsw.schoolpaper.jwt.JwtUtils;
+import kr.hs.gbsw.schoolpaper.student.domain.StudentEntity;
+import kr.hs.gbsw.schoolpaper.teacher.domain.TeacherEntity;
+import kr.hs.gbsw.schoolpaper.teacher.repository.TeacherRepository;
 import kr.hs.gbsw.schoolpaper.user.dto.UserLoginDto;
-import kr.hs.gbsw.schoolpaper.user.dto.UserRegisterDto;
-import kr.hs.gbsw.schoolpaper.user.entity.RoleEntity;
-import kr.hs.gbsw.schoolpaper.user.entity.UserEntity;
+import kr.hs.gbsw.schoolpaper.user.dto.UserStudentRegisterDto;
+import kr.hs.gbsw.schoolpaper.user.domain.RoleEntity;
+import kr.hs.gbsw.schoolpaper.user.domain.UserEntity;
+import kr.hs.gbsw.schoolpaper.user.dto.UserTeacherRegisterDto;
 import kr.hs.gbsw.schoolpaper.user.repository.RoleRepository;
+import kr.hs.gbsw.schoolpaper.user.repository.StudentRepository;
 import kr.hs.gbsw.schoolpaper.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.UUID;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository repository;
     private final RoleRepository roleRepository;
     private final UserRepository userRepository;
+    private final StudentRepository studentRepository;
     private final BCryptPasswordEncoder passwordEncoder;
     private final AuthenticationManager authenticationManager;
+    private final TeacherRepository teacherRepository;
     private final JwtUtils jwtUtils;
 
     public UserService(UserRepository repository, RoleRepository roleRepository, UserRepository userRepository, BCryptPasswordEncoder passwordEncoder, AuthenticationManager authenticationManager, JwtUtils jwtUtils) {
@@ -41,15 +48,20 @@ public class UserService {
     }
 
     public String login(UserLoginDto dto) {
-        Authentication result = authenticationManager.authenticate(new IdPwAuthentication(dto.getTel(), dto.getPassword()));
+
+        UserEntity user = repository.findByTel(dto.getTel()).orElseThrow(() -> new IllegalArgumentException(""));
+
+        Authentication result = authenticationManager.authenticate(new IdPwAuthentication(user.getUuid(), dto.getPassword()));
 
         return createJwtToken(result);
     }
 
-    public void register(UserRegisterDto dto) {
+    public void register(UserStudentRegisterDto dto) {
         if (repository.existsByTel(dto.getTel())) {
             throw new IllegalArgumentException("");
         }
+
+        StudentEntity student = studentRepository.findByGradeAndClassNumberAndStudentNumber(dto.getGrade(), dto.getClassNumber(), dto.getStudentNumber()).orElseThrow(() -> new IllegalArgumentException(""));
 
         RoleEntity role = roleRepository.findById("USER").orElseGet(() -> {
 
@@ -66,16 +78,17 @@ public class UserService {
         roles.add(role);
 
         UserEntity entity = new UserEntity();
-        entity.setName(dto.getName());
+        entity.setUuid(UUID.randomUUID());
         entity.setTel(dto.getTel());
         entity.setPassword(passwordEncoder.encode(dto.getPassword()));
         entity.setRoles(roles);
+        entity.setStudent(student);
 
         userRepository.save(entity);
     }
 
     public String createJwtToken(Authentication authentication) {
-        String tel = (String) authentication.getPrincipal();
+        String uuid = (String) authentication.getPrincipal();
 
         Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
 
