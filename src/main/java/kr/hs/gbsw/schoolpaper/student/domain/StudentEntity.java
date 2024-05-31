d20236ea8c2f6b8beffa92b7fb28579344517098
wjdgotjd <wjdgotjd529@naver.com>
2024-05-31 20:45:28 +0900

    학생 엔티티 추가

---
+++ src/main/java/kr/hs/gbsw/schoolpaper/student/domain/StudentEntity.java
@@ -0,0 +1,33 @@
+package kr.hs.gbsw.schoolpaper.student.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import kr.hs.gbsw.schoolpaper.user.domain.UserEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+
+@Entity
+@Table(name = "student")
+@Getter
+@Setter
+public class StudentEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idx;
+
+    private int grade;
+
+    private int classNumber;
+
+    private int studentNumber;
+
+    private String name;
+
+    @OneToOne(mappedBy = "student", cascade = {CascadeType.REMOVE})
+    @JsonIgnore
+    private UserEntity user;
+
+
+}
